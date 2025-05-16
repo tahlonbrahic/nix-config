2f8da2b015a0eb30964e2531f91ca9c0265dbab8
TahlonBrahic <tahlonbrahic@gmail.com>
2025-05-15 21:29:55 -0400

    create new config for athena

--- src/systems/athena/_configuration.nix
+++ src/systems/athena/_configuration.nix
@@ -1,36 +1,45 @@
 {
   inputs,
+  outPath,
   lib,
   ...
 }: {
   config = {
     system.includeBuildDependencies = lib.mkForce false;
-    networking.hostName = "athena";
-    users.users.tahlon.group = "tahlon";
-    users.groups.tahlon = {};
-    users.users.tahlon.isSystemUser = true;
+    networking.networkmanager.enable = true;
+
+    services.tailscale = {
+      enable = true;
+    };
+
+    security.pam.services.hyprlock = {};
+
     frostbite = {
       display.design.theme = "${inputs.assets}/themes/nord.yaml";
-      # email.address = "tahlonbrahic@proton.me";
-      #home-assistant = {
-      #  enable = true;
-      #  email = "tahlonbrahic@proton.me";
-      #  domain = "home.brahic.family";
-      #  #fqdn = "hass.brahic.family";
-      #};
       services.ssh = {
         publicKeys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDRUJCFyU2Bhag5GHGq2ihZL6LljX8EZygeKU6KDzHL8 tbrahic@proton.me"
         ];
       };
+      gaming.steam.enable = true;
       security = {
+        secrets.defaultSopsFile = outPath + "/src/secrets/secrets.yaml";
         useCase = "workstation";
         yubikey.enable = false;
+        SELinux.enable = false;
       };
-      networking.firewall.enable = false;
-      users.accounts = [
-        "tahlon"
-      ];
+
+      networking = {
+        enable = false;
+        firewall.enable = false;
+        fail2ban.enable = false;
+      };
+
+      users.users = {
+        tahlon = {
+          isAdministrator = lib.mkForce true;
+        };
+      };
     };
   };
 }
