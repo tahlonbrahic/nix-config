8b4d54d450477069a5c77e0f2750b23233a9a298
TahlonBrahic <tahlonbrahic@gmail.com>
2025-07-13 09:15:24 -0400

    systemd wireless dhcp and static staged

--- src/systems/yoru/_stage.nix
+++ src/systems/yoru/_stage.nix
@@ -1,28 +1,63 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   networking = {
-    useNetworkd = true;
     usePredictableInterfaceNames = true;
-    #resolvconf.enable = false;
-    #useHostResolvConf = false;
-    #tempAddresses = "disabled";
+    # allowAuxiliaryImperativeNetworks = true;
+    nameservers = ["8.8.8.8"];
+    tempAddresses = "disabled";
     enableIPv6 = false;
-    dhcpcd.enable = false;
     wireless = {
-      #enable = true;
-      iwd.enable = true;
+      iwd = {
+        enable = true;
+        settings = {
+          Network = {
+            EnableIPv6 = false;
+            RoutePriorityOffset = 300;
+            NameResolvingService = "systemd";
+          };
+          Settings = {
+            AutoConnect = true;
+          };
+        };
+      };
+      interfaces = ["wlan0" "wireless"];
     };
   };
 
   environment.systemPackages = [pkgs.iwgtk];
 
-  # systemd.network = {
-  #  enable = true;
-  #
-  #  networks = {
-  #    "10-wireless" = {
-  #      matchConfig.Name = "wlo1";
-  #      networkConfig.DHCP = "yes";
-  #    };
-  #  };
-  #};
+  services.resolved.enable = true;
+
+  systemd.network = {
+    enable = true;
+    wait-online.enable = false;
+
+    links = {
+      "10-rename-wlo1" = {
+        matchConfig.Path = "pci-0000:00:14.3*";
+        linkConfig.Name = "wireless";
+      };
+    };
+
+    networks = {
+      "25-wireless-static" = {
+        matchConfig = {
+          Name = "wireless";
+          SSID = "ATTp4pQVS2";
+        };
+        address = ["192.168.1.42/24"];
+        routes = [
+          {Gateway = "192.168.1.254";}
+        ];
+        dns = ["8.8.8.8"];
+        networkConfig = {
+          DHCP = "no";
+          LinkLocalAddressing = "no";
+        };
+      };
+    };
+  };
 }
